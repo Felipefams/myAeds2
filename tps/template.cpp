2d7051b7a0a947639f753553e9b920d6b47b2539
Felipefams <augustofams@gmail.com>
2022-03-14 18:37:41 -0300

    template fix

--- tps/template.cpp
+++ tps/template.cpp
@@ -14,8 +14,8 @@ void solve(){
 }
 
 int main(){
-	ios_base::syns_with_stdio(0);
-	cin.tie(0); cout.tie(0)
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
 	int t = 0;
 	cin >> t;
 	while(t--){
