525d9b5007751aa6f8851e9761e7e6f48e28e7a0
Felipefams <augustofams@gmail.com>
2022-03-15 21:54:13 -0300

    future features

---
+++ libs/myList.java
@@ -0,0 +1,19 @@
+public class myList{
+	private static length;
+
+	public static void push_back{
+
+	}
+	public static void insert{
+
+	}
+	public static void remove{
+
+	}
+	public static void setAt{
+
+	}
+	public static void change{
+
+	}
+}
