6d52b20b452d44dd5034519c858c256ed6f174c7
Felipefams <augustofams@gmail.com>
2022-03-18 20:24:27 -0300

    resumo somatorios inicio

---
+++ libs/all_in_one.java
@@ -0,0 +1,6 @@
+public class all_in_one.java{
+
+  public static void main(String[] args){
+
+  }
+}
---
+++ tps/tp02/ex01.java
@@ -0,0 +1,13 @@
+public class ex01{
+
+    public class filme{
+
+    }
+    public static class doThings(String s){
+
+    }
+
+    public static void main(String[] args){
+        
+    }
+}
