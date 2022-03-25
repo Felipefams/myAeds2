bef3f1b2b237a418696cc8a6f24fe67bfd1e5ed4
Felipefams <augustofams@gmail.com>
2022-03-25 17:27:18 -0300

    inicio movie

--- tps/tp02/entities/Movie.java
+++ tps/tp02/entities/Movie.java
@@ -1,5 +1,13 @@
 public class Movie{
-	private String movieName;
-	private String movieLength;
+	private String 	title;			//nome
+	private String 	originalTitle;	//titulo original
+	private Date 	releaseDate;	// data de lancamento
+	private int 	length;			//duracao
+	private String 	gender;			//genero do filme 
+	private String 	language;		//idioma original
+	private String 	state;			//situacao
+	private float 	budget;			//orcamento 
+	private String[] key;			//keywords
+	
 
 }
