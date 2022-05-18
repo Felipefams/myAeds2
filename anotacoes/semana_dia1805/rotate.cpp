8967ba78023d3b26da668374eba5e6aa8d520a5c
felipefams <augustofams@gmail.com>
2022-05-18 10:34:31 -0300

    aula 18-05

---
+++ anotacoes/semana_dia1805/rotate.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int element;
+    int level;
+    Node l;
+    Node d;
+    Node(int element){}; 
+
+    
+}
+
+int main(void){
+
+    return(0);
+}
